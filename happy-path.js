6feffbd58a3dc5d5a8eab3dd8975a403da2e95ed
BosEriko <bos.eriko@gmail.com>
2020-07-15 09:46:49 +0800

    add happy path and remove example.png

---
+++ happy-path.js
@@ -0,0 +1,64 @@
+const assert = require("chai").assert;
+const puppeteer = require("puppeteer");
+
+const URL = 'http://localhost:3000';
+const EMAIL = 'dev@editmode.app';
+const PASSWORD = 'password';
+
+(async () => {
+  // Authenticate
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const navigationPromise = page.waitForNavigation();
+
+  await page.goto(`${URL}/users/sign_in`);
+  await page.type('input[type="email"]', EMAIL);
+  await page.type('input[type="password"]', PASSWORD);
+  await page.click('button[type="submit"]');
+
+  // Create a new Project
+  await page.waitFor(3000);
+  const timestamp = +(new Date());
+  const newProjectButton = ".flex > .flex-1 > .max-w-3xl > .pt-8 > .white";
+  const projectNameInput = '#project_name';
+
+  await page.waitForSelector(newProjectButton);
+  await page.click(newProjectButton);
+
+  await page.waitFor(3000);
+
+  await page.waitForSelector(projectNameInput);
+  await page.type(projectNameInput, `${timestamp}-project`);
+
+  await page.click('button[type="submit"]');
+
+  // Delete the project
+  await page.goto(`${URL}/projects`);
+  const createdProject = "ul > li:last-child > a";
+
+  await page.waitForSelector(createdProject);
+  await page.click(createdProject);
+
+  await page.waitFor(3000);
+
+  const projectSettingsButton = '#project-settings-button';
+  const projectDeleteButton = '#delete-project-button';
+
+  await page.waitForSelector(projectSettingsButton);
+  await page.click(projectSettingsButton);
+
+  await page.waitFor(3000);
+  await page.screenshot({ path: './tmp/settings-button-click.jpg', type: 'jpeg' });
+
+  await page.waitForSelector(projectDeleteButton);
+  await page.click(projectDeleteButton);
+
+  await page.waitFor(3000);
+  await page.screenshot({ path: './tmp/delete-button-click.jpg', type: 'jpeg' });
+  await page.keyboard.press(String.fromCharCode(13));  // Simulate a pressing of the Return key
+
+  await navigationPromise;
+
+  // Close the browser
+  await browser.close();
+})();
